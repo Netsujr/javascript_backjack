3b57fb0bdd957ef56d09bace0987920bf52d0e1b
Renato Delboni <renatonato_jr@hotmail.com>
2021-12-29 16:59:04 +0900

    added gif paths, not working.
    will finish game logic first before retrying this

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ let blackjackGame = {
   'dealer': { 'scoreSpan': '#dealer-score', 'div': '#dealer-box', 'score': 0 },
   'cards': ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'],
   'cardsMap': { '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 10, 'Q': 10, 'K': 10, 'A': [1, 11] },
+  'win': ['bman', 'dk', 'dk2', 'dk3', 'kirby', 'kirby2', 'link', 'marioK'],
+  'lose': [],
+  'draw': []
 };
 
 const YOU = blackjackGame['you'];
@@ -21,14 +24,13 @@ document.querySelector('#deal-button').addEventListener('click', Deal);
 // *************** ButtonFunctions ******************
 
 function Hit() {
-  // console.log('Hit was Hit');
   let card = randomCard();
   showCard(card, YOU);
-  console.log(card);
   updateScore(card, YOU);
-  console.log(YOU['score']);
   showScore(YOU);
-  // showCard(DEALER);
+  // console.log('Hit was Hit');
+  // console.log(card);
+  // console.log(YOU['score']);
 }
 
 function Stand() {
@@ -37,9 +39,10 @@ function Stand() {
 }
 
 function Deal() {
-  // showResult(decideWinner());
-  // decideWinner();
-  // console.log('Deal was Hit');
+  showResult(decideWinner());
+  // let winner = decideWinner();
+  // showResult(winner);
+  // ------------------
   let yourImages = document.querySelector('#your-box').querySelectorAll('img');
   let dealerImages = document.querySelector('#dealer-box').querySelectorAll('img');
 
@@ -142,11 +145,10 @@ function decideWinner() {
   return winner;
 }
 
-function showResult(winner, gif) {
+function showResult(winner) {
   let message, messageColor;
-
   if (winner === YOU) {
-    message = "You Won!";
+    message = `You Won!`;
     messageColor = "green";
     winSound.play();
 
@@ -161,6 +163,17 @@ function showResult(winner, gif) {
     loseSound.play();
   }
 
-document.querySelector('#blackjack-result'). textContent = message;
-document.querySelector('#blackjack-result'). style.color = messageColor;
+  document.querySelector('#blackjack-result').textContent = message;
+  document.querySelector('#blackjack-result').style.color = messageColor;
 }
+
+// ************gifs************
+
+// function randomWinnerGif() {
+//   let randomIndex = Math.floor(Math.random() * 8);
+//   return blackjackGame['win'][randomIndex];
+// }
+
+// let gifImage = document.createElement('img');
+// let winGif = randomWinnerGif();
+// gifImage.src = `gifs/win/${gif}.gif`;
