4993c8fc852174d8bbb22b10416f6eb487020e85
Renato Delboni <renatonato_jr@hotmail.com>
2021-12-29 11:27:53 +0900

    wont count Ace equaling 1 just yet. All other cards work and show up fine

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 // *************** Variables ******************
 let blackjackGame = {
   'you': { 'scoreSpan': '#your-score', 'div': '#your-box', '#score': 0 },
-  'dealer': { 'scoreSpan': '#dealer-score', 'div': '#dealer-box', '#score': 0 }
+  'dealer': { 'scoreSpan': '#dealer-score', 'div': '#dealer-box', '#score': 0 },
+  'cards': ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'],
 };
 
 const YOU = blackjackGame['you'];
 const DEALER = blackjackGame['dealer'];
-
 const hitSound = new Audio('sounds/swish.m4a');
-
-
 
 // *************** Listeners ******************
 
@@ -17,26 +15,15 @@ document.querySelector('#hit-button').addEventListener('click', Hit);
 document.querySelector('#stand-button').addEventListener('click', Stand);
 document.querySelector('#deal-button').addEventListener('click', Deal);
 
-
-
-
-
-// *************** Functions ******************
+// *************** ButtonFunctions ******************
 
 function Hit() {
   // console.log('Hit was Hit');
-  showCard(YOU);
+  let card = randomCard();
+  showCard(card, YOU);
+  console.log(card);
   // showCard(DEALER);
 }
-
-function showCard(activePlayer) {
-  let cardImage = document.createElement('img');
-  cardImage.src = 'images/cards/Q.png';
-  document.querySelector(activePlayer['div']).appendChild(cardImage);
-  hitSound.play();
-}
-
-
 
 function Stand() {
   // console.log('Stand was Hit');
@@ -55,4 +42,19 @@ function Deal() {
   for (image = 0; image < dealerImages.length; image++) {
     dealerImages[image].remove();
   }
+}
+
+
+// *************** Logic Functions ******************
+
+function showCard(card, activePlayer) {
+  let cardImage = document.createElement('img');
+  cardImage.src = `images/cards/${card}.png`;
+  document.querySelector(activePlayer['div']).appendChild(cardImage);
+  hitSound.play();
+}
+
+function randomCard() {
+  let randomIndex = Math.floor(Math.random() * 13);
+  return blackjackGame['cards'][randomIndex];
 }
