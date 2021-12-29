e15e3410ead056b0148b49874743ce30a48eb45b
Renato Delboni <renatonato_jr@hotmail.com>
2021-12-29 11:01:58 +0900

    hit button works
    made flexible by refactoring

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
+// *************** Variables ******************
+let blackjackGame = {
+  'you': { 'scoreSpan': '#your-score', 'div': '#your-box', '#score': 0 },
+  'dealer': { 'scoreSpan': '#dealer-score', 'div': '#dealer-box', '#score': 0 }
+};
+
+const YOU = blackjackGame['you'];
+const DEALER = blackjackGame['dealer'];
+
+const hitSound = new Audio('sounds/swish.m4a');
+
+
+
+// *************** Listeners ******************
+
 document.querySelector('#hit-button').addEventListener('click', Hit);
+document.querySelector('#stand-button').addEventListener('click', Stand);
+document.querySelector('#deal-button').addEventListener('click', Deal);
 
 
 
@@ -8,5 +25,24 @@ document.querySelector('#hit-button').addEventListener('click', Hit);
 
 function Hit() {
   // console.log('Hit was Hit');
+  showCard(YOU);
+}
+
+function showCard(activePlayer) {
+  let cardImage = document.createElement('img');
+  cardImage.src = 'images/cards/Q.png';
+  document.querySelector(activePlayer['div']).appendChild(cardImage);
+  hitSound.play();
+}
+
+
+
+function Stand() {
+  // console.log('Stand was Hit');
+
+}
+
+function Deal() {
+  // console.log('Deal was Hit');
 
 }
